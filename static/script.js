43050d049d58d8c6cea57441ac2fa4d25313aeb5
Sami Ullah <sk2579784@gmail.com>
2025-01-27 17:50:52 +0500

    Remove unnecessary files from the repository

--- static/script.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener('scroll', function() {
-//     const header = document.getElementById('header');
-//     if (window.scrollY > 0) {
-//       header.style.backgroundColor = 'transparent';
-//     }
-//   });
-  
